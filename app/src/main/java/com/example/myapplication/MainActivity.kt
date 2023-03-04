c0c15daa477848032d4f867d8df0058dd837ada0
valentine <valentinetuyrin@gmail.com>
2023-03-04 19:57:22 +0300

    no message

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val adapter = PlantAdapter()
+    private val imageIdList = listOf(
+        R.drawable.plant1,
+        R.drawable.plant2,
+        R.drawable.plant3,
+        R.drawable.plant4,
+        R.drawable.plant5
+    )
+    private var index = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+
     }
+
+    private fun init() {
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView.adapter = adapter
+            buttonAdd.setOnClickListener {
+                if (index > 4) index = 0
+                val plant = Plant(imageIdList[index], "Plant $index")
+                adapter.addPlant(plant)
+                index++
+            }
+        }
+    }
+
 }
